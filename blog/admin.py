bace04f98d955256cf33ec331be961292c308d8a
S.Amirhosein.M <s.a.mousavi647@gmail.com>
2026-02-03 14:50:14 +0330

    added post table in blog app

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from blog.models import Post, Category
 
 # Register your models here.
+
+admin.site.register(Category)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status', 'author', 'counted_view', 'published_date', 'created_date']
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty'
+    list_filter = ('author', 'status', 'category')
+    search_fields = ['title', 'content']
+
+    class Meta:
+        ordering = ['-created_date']
+
+
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=63)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=127)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    image = models.ImageField(upload_to='blog/', default='blog/default.jpg')
+    # tag
+    category = models.ManyToManyField(Category)
+    counted_view = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+    
+
