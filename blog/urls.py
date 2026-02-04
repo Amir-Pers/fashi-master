2c18d3f0f36fa676883dac99687d542b6428e4ff
S.Amirhosein.M <s.a.mousavi647@gmail.com>
2026-02-04 15:10:13 +0330

    fixed blog-details with dynamic data

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,7 @@ app_name = 'blog'
 urlpatterns = [
     path('', blog, name='index'),
     path('blog-details/', blog_details, name='blog-details'),
+    path('blog-details/<int:pid>/', blog_details, name='blog-details'),
+    path('category/<str:cat_name>/', blog, name='category'),
+    path('author/<str:author_username>/', blog, name='author'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from blog.models import Post, Category 
 
 # Create your views here.
 
-def blog(request):
+def blog(request, **kwargs):
     posts = Post.objects.filter(status=1).order_by('-published_date')
+    
+    if kwargs.get('cat_name') != None:
+        posts = posts.filter(category__name=kwargs['cat_name'])
+
+    if kwargs.get('author_username') != None:
+        posts = posts.filter(author__username=kwargs['author_username'])
+
     context = {
         'posts' : posts
     }
+
     return render(request, 'blog/blog-home.html', context)
 
 
-def blog_details(request):
-    return render(request, 'blog/blog-details.html')
+def blog_details(request, **kwargs):
+    pid = kwargs.get('pid')
+    posts = Post.objects.filter(status=1)
+    if not pid:
+        raise Http404('Post Not Found')
+
+    post = get_object_or_404(posts, pk=pid)
+    next_post = posts.filter(published_date__gt=post.published_date).order_by('published_date').first()
+    prev_post = posts.filter(published_date__lt=post.published_date).order_by('-published_date').first()
+
+    context = {
+        'post' : post,
+        'next_post' : next_post,
+        'prev_post' : prev_post,
+    }
+    return render(request, 'blog/blog-details.html', context)
 
