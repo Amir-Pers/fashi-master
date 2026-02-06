f5401a8ab5e27726fa71c3f6ab99a72339b980f4
S.Amirhosein.M <s.a.mousavi647@gmail.com>
2026-02-06 18:42:58 +0330

    added dynamic category and recent post to blog

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,30 @@
+from django import template
+from blog.models import Post, Category
+
+register = template.Library()
+
+
+@register.simple_tag()
+def function(a):
+    return a + 2 
+
+@register.simple_tag()
+def counter():
+    count = Post.objects.filter(status=1).count()
+    return count
+
+@register.inclusion_tag('blog/blog-recent.html')
+def recent_posts(arg=4):
+    posts = Post.objects.filter(status=1).order_by('-published_date')[:arg]
+    return {'posts' : posts}
+
+@register.inclusion_tag('blog/blog-category.html')
+def categories():
+    cats = Category.objects.all()
+    posts = Post.objects.filter(status=1)
+    cat_dict = dict()
+    for name in cats:
+        if posts.filter(category=name).count() > 0:
+            cat_dict[name] = posts.filter(category=name).count()
+    return {'categories' : cat_dict}
+    
