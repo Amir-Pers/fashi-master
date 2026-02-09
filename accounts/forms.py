d314d4a6456c890e853025a737d9163eb3ac06e5
S.Amirhosein.M <s.a.mousavi647@gmail.com>
2026-02-09 12:01:02 +0330

    fixed login view

---
+++ accounts/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import get_user_model
+from django.db.models import Q
+
+User = get_user_model()
+
+class EmailOrUsernameAuthenticationForm(AuthenticationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'autofocus': True, 'placeholder': 'Username or Email'})
+    )
+    password = forms.CharField(
+    widget=forms.PasswordInput(attrs={'placeholder': 'Password'})
+    )
+
+
+    def clean(self):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+
+        if username and password:
+            try:
+                user = User.objects.get(Q(username=username) | Q(email=username))
+            except User.DoesNotExist:
+                raise forms.ValidationError('Please enter a correct username and password. Note that both fields may be case-sensitive.')
+
+            if not user.check_password(password):
+                raise forms.ValidationError('Please enter a correct username and password. Note that both fields may be case-sensitive.')
+
+            self.user_cache = user
+
+        return self.cleaned_data
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import EmailOrUsernameAuthenticationForm
+from django.contrib import messages
 
 # Create your views here.
+
 def register(request):
     return render(request, 'accounts/register.html')
 
+
 def login_view(request):
-    return render(request, 'accounts/login.html')
+    if request.user.is_authenticated:
+        return redirect('/')
+
+    if request.method == "POST":
+        form = EmailOrUsernameAuthenticationForm(request=request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/')
+
+    else:
+        form = EmailOrUsernameAuthenticationForm()
+
+    return render(request, 'accounts/login.html', {'form': form})
+
 
 def my_account(request):
     return render(request, 'accounts/my-account.html')
